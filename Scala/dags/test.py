3ffdff0eeb5badfe2f6bf5a232a8e3155d3531d6
Anton <anton.goridko@icloud.com>
2025-11-12 15:38:05 +0100

    update

--- Scala/dags/test.py
+++ Scala/dags/test.py
@@ -26,7 +26,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id="compact_parquet_docker",
+    dag_id="compact_parquet_docker_test",
     default_args=default_args,
     schedule_interval="0 2 * * *",   # ежедневно в 02:00 UTC
     catchup=False,
